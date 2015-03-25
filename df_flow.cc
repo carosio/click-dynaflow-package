9a1bce334dc318588fdc61fbeb86404e3fd5d768
Tobias Steinicke <tobias.steinicke@travelping.com>
2015-03-25 19:34:04 +0000

    finishing Flow

--- df_flow.cc
+++ df_flow.cc
@@ -1,26 +1,32 @@
 #include <click/config.h>
 #include <click/packet.hh>
+#include <clicknet/ip.h>
+#include <clicknet/udp.h>
 #include "df_flow.hh"
 #include "jhash.hh"
 
 CLICK_DECLS
 
-IdManager flows_ids;
-
-Flow::Flow(Packet *p, int offset)
+Flow::Flow(Packet *p)
 {
-    data = FlowData(p->ip_header()->ip_src.s_addr, p->ip_header()->ip_dst.s_addr, ); // ports , proto
+    const click_ip *iph = p->ip_header();
+    const click_udp *udph = p->udp_header();
+    assert(p->has_network_header() && p->has_transport_header() && IP_FIRSTFRAG(iph));
+    data = new FlowData(iph->ip_src.s_addr, iph->ip_dst.s_addr, udph->uh_sport, udph->uh_dport, iph->ip_p);
+    _id = hash();
+    srcFlow = this;
+}
+
+Flow::Flow(FlowData *fd)
+{
+    data = fd;
     _id = hash();
     srcFlow = this;
 }
 
 Flow::~Flow()
 {
-}
-
-uint32_t
-Flow::hash() {
-    return jhash(data);
+    delete data;
 }
 
 CLICK_ENDDECLS
--- df_flow.hh
+++ df_flow.hh
@@ -3,6 +3,7 @@
 #include <click/packet.hh>
 #include <click/bighashmap.hh>
 #include "df.hh"
+#include "jhash.hh"
 
 CLICK_DECLS
 
@@ -16,19 +17,22 @@ struct FlowData {
     FlowData(uint32_t src_ipv4_, uint32_t dst_ipv4_, uint16_t src_port_, uint16_t dst_port_, uint8_t proto_) :
         src_ipv4(src_ipv4_), dst_ipv4(dst_ipv4_), src_port(src_port_), dst_port(dst_port_), proto(proto_) {};
 
-    inline FlowData reverse() { return FlowData(dst_ipv4, src_ipv4, dst_port, src_port, proto); };
+    inline FlowData * reverse() { return new FlowData(dst_ipv4, src_ipv4, dst_port, src_port, proto); };
 };
 
 struct Flow {
-    FlowData data;
+    FlowData  * data;
     Flow * srcFlow;
 
     Flow(Packet *);
+    Flow(FlowData *);
+    ~Flow();
 
     inline bool operator==(Flow other) { return _id == other._id; }
     inline uint32_t src_id() { return srcFlow->_id; };
+    inline uint32_t hash() { return jhash(&data, sizeof(FlowData), 0); };
+    inline Flow * reverse() { return new Flow(data->reverse()); };
 
-  private:
     uint32_t _id;
 };
 
