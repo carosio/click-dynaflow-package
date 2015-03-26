74fe32c1b99cf2d9cd67e8c0334911770ede2e7e
Tobias Steinicke <tobias.steinicke@travelping.com>
2015-03-26 20:50:23 +0000

    Flows, extended and hash collision handling.
    Flow is now saving possible needed informations, like
    - Action
    - Client
    - source Group
    - destination Group
    
    To handle hash collisions, behind each ID is Vector of Flows.
    With that the pure mapping betwenn Flow IDs and Actions are gone.

--- df_flow.cc
+++ df_flow.cc
@@ -3,9 +3,27 @@
 #include <clicknet/ip.h>
 #include <clicknet/udp.h>
 #include "df_flow.hh"
+#include "df_clients.hh"
+#include "df.hh"
 #include "jhash.hh"
 
 CLICK_DECLS
+
+Flow::Flow(Packet *p, ClientValue *client_, uint32_t srcGroup_, uint32_t dstGroup_, DF_RuleAction action_)
+{
+    const click_ip *iph = p->ip_header();
+    const click_udp *udph = p->udp_header();
+    assert(p->has_network_header() && p->has_transport_header() && IP_FIRSTFRAG(iph));
+    data = new FlowData(iph->ip_src.s_addr, iph->ip_dst.s_addr, udph->uh_sport, udph->uh_dport, iph->ip_p);
+    _id = hash();
+    srcFlow = this;
+    client = client_;
+    srcGroup = srcGroup_;
+    dstGroup = dstGroup_;
+    action = action_;
+    _count = 0;
+    _count_valid = false;
+}
 
 Flow::Flow(Packet *p)
 {
@@ -15,13 +33,25 @@ Flow::Flow(Packet *p)
     data = new FlowData(iph->ip_src.s_addr, iph->ip_dst.s_addr, udph->uh_sport, udph->uh_dport, iph->ip_p);
     _id = hash();
     srcFlow = this;
+    client = NULL;
+    srcGroup = 0;
+    dstGroup = 0;
+    action = DF_RULE_UNKNOWN;
+    _count = 0;
+    _count_valid = false;
 }
 
-Flow::Flow(FlowData *fd)
+Flow::Flow(FlowData *fd, ClientValue *client_, uint32_t srcGroup_, uint32_t dstGroup_, DF_RuleAction action_)
 {
     data = fd;
     _id = hash();
     srcFlow = this;
+    client = client_;
+    srcGroup = srcGroup_;
+    dstGroup = dstGroup_;
+    action = action_;
+    _count = 0;
+    _count_valid = false;
 }
 
 Flow::~Flow()
@@ -29,5 +59,43 @@ Flow::~Flow()
     delete data;
 }
 
+Flow * FlowHashEntry::get(Flow *f) {
+    for (FlowVector::const_iterator it = fv.begin(); it != fv.end(); ++it) {
+        if ((*it) == f)
+            return (*it);
+    }
+    return NULL;
+}
+
+Flow * FlowHashEntry::get(uint32_t count) {
+    for (FlowVector::const_iterator it = fv.begin(); it != fv.end(); ++it) {
+        if ((*it)->_count == count)
+            return (*it);
+    }
+    return NULL;
+}
+
+void FlowHashEntry::add(Flow *f) {
+    fv.push_back(f);
+    f->_count = ids.AllocateId();
+    f->_count_valid = true;
+
+    return;
+}
+
+Flow * FlowHashEntry::remove(Flow *f) {
+    Flow * tmpf;
+    for (FlowVector::iterator it = fv.begin(); it != fv.end(); ++it) {
+        if ((*it) == f) {
+            tmpf = *(fv.erase(it));
+            ids.FreeId(tmpf->_count);
+            tmpf->_count_valid = false;
+        }
+    }
+
+    return NULL;
+}
+
 CLICK_ENDDECLS
 ELEMENT_PROVIDES(Flow)
+ELEMENT_PROVIDES(FlowHashEntry)
--- df_flow.hh
+++ df_flow.hh
@@ -2,7 +2,10 @@
 #define DF_FLOW_HH
 #include <click/packet.hh>
 #include <click/bighashmap.hh>
+#include <click/vector.hh>
 #include "df.hh"
+#include "df_clients.hh"
+#include "uniqueid.hh"
 #include "jhash.hh"
 
 CLICK_DECLS
@@ -18,26 +21,57 @@ struct FlowData {
         src_ipv4(src_ipv4_), dst_ipv4(dst_ipv4_), src_port(src_port_), dst_port(dst_port_), proto(proto_) {};
 
     inline FlowData * reverse() { return new FlowData(dst_ipv4, src_ipv4, dst_port, src_port, proto); };
+    inline bool operator==(FlowData other) { return (src_ipv4 == other.src_ipv4)
+                                             && (dst_ipv4 == other.dst_ipv4)
+                                             && (src_port == other.src_port)
+                                             && (dst_port == other.dst_port)
+                                             && (proto == other.proto); };
 };
 
 struct Flow {
     FlowData  * data;
     Flow * srcFlow;
 
+    Flow(Packet *, ClientValue *, uint32_t, uint32_t, DF_RuleAction);
     Flow(Packet *);
-    Flow(FlowData *);
+    Flow(FlowData *, ClientValue *, uint32_t, uint32_t, DF_RuleAction);
     ~Flow();
 
-    inline bool operator==(Flow other) { return _id == other._id; }
+    inline bool operator==(Flow other) { return data == other.data; };
     inline uint32_t src_id() { return srcFlow->_id; };
     inline uint32_t hash() { return jhash(&data, sizeof(FlowData), 0); };
-    inline Flow * reverse() { return new Flow(data->reverse()); };
+    inline Flow * reverse() { return new Flow(data->reverse(),
+                                           client, srcGroup, dstGroup, action); };
 
+    ClientValue *client;
+    uint32_t srcGroup;
+    uint32_t dstGroup;
+    DF_RuleAction action;
     uint32_t _id;
+    uint32_t _count;
+    bool _count_valid;
 };
 
-typedef HashMap<uint32_t, Flow *> FlowTable;
-typedef HashMap<uint32_t, DF_RuleAction> FlowActionTable;
+typedef Vector<Flow *> FlowVector;
+
+class FlowHashEntry {
+  public:
+
+    FlowHashEntry() {};
+    ~FlowHashEntry() {};
+
+    Flow * get(Flow *f);
+    Flow * get(uint32_t count);
+    void add(Flow *f);
+    Flow * remove(Flow *f);
+
+  private:
+
+    FlowVector fv;
+    IdManager ids;
+};
+
+typedef HashMap<uint32_t, FlowHashEntry *> FlowTable;
 
 CLICK_ENDDECLS
 #endif
